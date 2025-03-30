18f7af1a7155a749e06d379e0bb67e190e4efb10
Ismael Sanz <ismaespaik@gmail.com>
2025-03-30 14:33:18 +0200

    feat(package.json): removed unused libraries

--- client/jest.setup.ts
+++
@@ -1,5 +0,0 @@
-import "@testing-library/jest-dom";
-import fetchMock from "jest-fetch-mock";
-fetchMock.enableMocks();
-global.TextEncoder = require("util").TextEncoder;
-global.TextDecoder = require("util").TextDecoder;
